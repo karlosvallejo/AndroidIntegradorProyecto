1a99cf2b17a58c6f96a365655fcda558ac85dead
sebasif <sebassanchez12@hotmail.com>
2016-09-18 23:55:36 -0500

    Register

--- app/src/main/java/com/example/a1107513806/myapplication/Register.java
+++ app/src/main/java/com/example/a1107513806/myapplication/Register.java
@@ -1,10 +1,15 @@
 package com.example.a1107513806.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -18,13 +23,81 @@ public class Register extends AppCompatActivity implements Observer {
     private EditText contrase;
     private EditText email;
     private Button regis;
-
+    private String ipServidor;
+    private String getNombre;
+    private String getContrasena;
+    private String getEmail;
+    private Usuario user;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Intent intente = getIntent();
+        SerializameEsta.getInstance(ipServidor).addObserver(this);
+        ipServidor = intente.getStringExtra("ipeson");
 
+        if(nombre.getText().toString().equalsIgnoreCase("nombre")) {
+            nombre.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+                    nombre.setText("");
+
+                }
+            });
+
+        }
+
+
+        nombre.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent evente) {
+                if(actionId== EditorInfo.IME_ACTION_DONE){
+                    System.out.println("hecho");
+                    getNombre=nombre.getText().toString();
+                    System.out.println(getNombre);
+                    return false;
+                }
+
+
+                return true;
+            }
+        });
+
+        contrase.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId== EditorInfo.IME_ACTION_DONE){
+                    System.out.println("hecho");
+                    getContrasena=contrase.getText().toString();
+                    System.out.println(getContrasena);
+
+                    return false;
+                }
+
+
+                return true;
+            }
+        });
+
+        email.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId== EditorInfo.IME_ACTION_DONE){
+
+                    getEmail=email.getText().toString();
+
+
+                    return false;
+                }
+
+
+                return true;
+            }
+        });
         nombre = (EditText) findViewById(R.id.username);
         contrase = (EditText) findViewById(R.id.contra);
         email = (EditText) findViewById(R.id.email);
@@ -33,20 +106,25 @@ public class Register extends AppCompatActivity implements Observer {
         regis.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                String name = nombre.getText().toString();
-                String correo = email.getText().toString();
-                String contrasena = contrase.getText().toString();
-                Usuario usuario = new Usuario(name,contrasena);
+                getNombre=nombre.getText().toString();
+                getContrasena=contrase.getText().toString();
+                getEmail=email.getText().toString();
+
+                user= new Usuario(getNombre,getContrasena,getEmail);
+                SerializameEsta.getInstance(ipServidor).enviar(user);
 
                 }
+
+
         }
     }
-
-
-
-
-            @Override
+    @Override
     public void update(Observable observable, Object data) {
 
     }
 }
+
+
+
+
+
--- app/src/main/java/com/example/a1107513806/myapplication/Usuario.java
+++ app/src/main/java/com/example/a1107513806/myapplication/Usuario.java
@@ -7,10 +7,17 @@ import java.io.Serializable;
 public class Usuario implements Serializable{
     String nombre;
     String contrasena;
+    String email;
 
     public Usuario(String nombre, String contrasena){
         this.nombre=nombre;
         this.contrasena=contrasena;
+    }
+
+    public Usuario(String nombre, String contrasena, String email){
+        this.nombre=nombre;
+        this.contrasena=contrasena;
+        this.email=email;
     }
 
 
