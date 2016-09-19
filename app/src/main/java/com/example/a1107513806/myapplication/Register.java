0ec6579ae486e385cecbe8f89800845b048d8166
sebasif <sebassanchez12@hotmail.com>
2016-09-18 23:06:31 -0500

    Register

---
+++ app/src/main/java/com/example/a1107513806/myapplication/Register.java
@@ -0,0 +1,20 @@
+package com.example.a1107513806.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by sebas on 18/9/16.
+ */
+public class Register extends AppCompatActivity implements Observer {
+
+
+
+
+    @Override
+    public void update(Observable observable, Object data) {
+
+    }
+}
